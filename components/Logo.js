ad10b618be6426aa5f717d2c358c48ba02a74bc8
cassinodari <cassi.nodari@hotmail.com>
2019-03-16 10:36:15 -0300

    first commit

---
+++ components/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Image } from 'react-native'
+export default class Logo extends React.Component {
+    render() {
+        return (
+            <Image
+                source={require('../assets/logo.png')}
+            />
+        )
+    }
+}
